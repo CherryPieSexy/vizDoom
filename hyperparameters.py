731bc6fdf1e2f59172f999e649b874e747d71451
d.akimov <d.akimov@corp.mail.ru>
2018-05-08 07:44:23 +0300

    hp change

--- hyperparameters.py
+++ hyperparameters.py
@@ -53,7 +53,7 @@ hp_def_c_train = HyperParametersTrain(scenario='defend_the_center', config_file=
                                       log_folder='logs/Defend_The_Center/DQN/', cuda=True,
                                       learning_rate=0.0025,
                                       n_epoch=30, steps_per_epoch=5000, play_steps=10,
-                                      batch_size=64, time_size=10, tests_per_epoch=30,
+                                      batch_size=64, time_size=5, tests_per_epoch=30,
                                       start_epsilon=1.0, end_epsilon=0.1
                                       )
 hp_def_c_test = HyperParametersTest(scenario='defend_the_center', config_file='scenarios/defend_the_center.cfg',
@@ -67,7 +67,7 @@ hp_d_cor_train = HyperParametersTrain(scenario='deadly_corridor', config_file='s
                                       log_folder='logs/Deadly_Corridor/DQN/', cuda=True,
                                       learning_rate=0.0025,
                                       n_epoch=30, steps_per_epoch=5000, play_steps=10,
-                                      batch_size=64, time_size=10, tests_per_epoch=30,
+                                      batch_size=64, time_size=5, tests_per_epoch=30,
                                       start_epsilon=1.0, end_epsilon=0.1
                                       )
 hp_d_cor_test = HyperParametersTest(scenario='deadly_corridor', config_file='scenarios/deadly_corridor.cfg',
