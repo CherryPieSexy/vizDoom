Bug fix in drqn, add gradient clipping

models/drqn.py
@@ -21,7 +21,7 @@ class DRQN(nn.Module):
             self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
             self.conv3 = nn.Conv2d(64, 64, kernel_size=3)
 
-            self.lstm = nn.LSTM(2560, 512)
+            self.lstm = nn.LSTM(2560, 512, batch_first=True)
             self.fc = nn.Linear(512, n_actions)
 
     def forward(self, x_screens, hidden):

trainer.py
@@ -186,7 +186,7 @@ class Trainer:
 
     def _train_step(self, loss):
         self._optimizer.zero_grad()
-        # TODO: add gradient clipping in future
+        torch.nn.utils.clip_grad_norm_(self._policy_net.parameters, 1.0)
         loss.backward()
         self._optimizer.step()
 